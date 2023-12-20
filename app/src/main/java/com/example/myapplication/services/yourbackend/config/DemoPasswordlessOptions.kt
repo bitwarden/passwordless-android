858555186c34ea73726cf168989f41d403702348
Shubham Shankaram <56815364+shubhamji88@users.noreply.github.com>
2023-12-20 15:13:47 +0530

    Doc: Updated documentations of classes (#12)

--- app/src/main/java/com/example/myapplication/services/yourbackend/config/DemoPasswordlessOptions.kt
+++ app/src/main/java/com/example/myapplication/services/yourbackend/config/DemoPasswordlessOptions.kt
@@ -1,5 +1,13 @@
 package com.example.myapplication.services.yourbackend.config
-
+/**
+ * @property API_URL The Passwordless.dev server url.
+ * @property API_KEY Your public API key.
+ * @property RP_ID This stands for “relying party”; it can be considered as describing the organization responsible for registering and authenticating the user.
+ * Set this as base url for your backend. So, https://<Relying Party ID>/.well-known/assetlinks.json is accessible
+ * @property ORIGIN This is your generated key for your app, refer readme on how to generate this.
+ * String Format: "android:apk-key-hash:<Hash value>" , Example "android:apk-key-hash:NX7853gQH6KKGF4iT7WmpEtBDw7njd75WuaAFKzyW44"
+ * @property YOUR_BACKEND_URL This is where your backend is hosted.
+ */
 class DemoPasswordlessOptions {
     companion object {
         const val API_KEY = "yourapp:public:ab2e4350d43946f7b4c93d98fa2c765e"
--- passwordless/src/main/java/dev/passwordless/android/rest/PasswordlessOptions.kt
+++ passwordless/src/main/java/dev/passwordless/android/rest/PasswordlessOptions.kt
@@ -7,8 +7,8 @@ import dev.passwordless.android.rest.converters.Base64UrlConverter
  * @property apiKey Your public API key.
  * @property rpId This stands for “relying party”; it can be considered as describing the organization responsible for registering and authenticating the user.
  * @property origin This is where your backend is hosted.
- * @property
  */
+
 data class PasswordlessOptions(
     val apiKey: String,
     val rpId: String,
