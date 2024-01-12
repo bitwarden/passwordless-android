e93aece6079cffa18d7fc74eb423f6764242acfc
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-01-12 09:19:18 +0100

    ActivityScoped instead of singleton so we can inject Activity's Context (#21)

--- app/src/main/java/com/example/myapplication/di/AppModule.kt
+++ app/src/main/java/com/example/myapplication/di/AppModule.kt
@@ -1,35 +1,34 @@
 package com.example.myapplication.di
 
+import android.content.Context
 import com.example.myapplication.services.yourbackend.YourBackendHttpClientFactory
 import com.example.myapplication.services.yourbackend.config.DemoPasswordlessOptions
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.qualifiers.ActivityContext
+import dagger.hilt.android.scopes.ActivityScoped
 import dev.passwordless.android.PasswordlessClient
 import dev.passwordless.android.rest.PasswordlessOptions
-import javax.inject.Singleton
 
 @Module
-@InstallIn(SingletonComponent::class)
-object AppModule {
-
-    // Provided a singleton object of PasswordlessClient
-
+@InstallIn(ActivityComponent::class)
+class AppModule {
     @Provides
-    @Singleton
-    fun providePasswordlessClient(): PasswordlessClient {
+    @ActivityScoped
+    fun providePasswordlessClient(@ActivityContext activity: Context): PasswordlessClient {
         val options = PasswordlessOptions(
             DemoPasswordlessOptions.API_KEY,
             DemoPasswordlessOptions.RP_ID,
             DemoPasswordlessOptions.ORIGIN,
             DemoPasswordlessOptions.API_URL
         )
-        return PasswordlessClient(options)
+        return PasswordlessClient(options, activity)
     }
 
     @Provides
-    @Singleton
+    @ActivityScoped
     fun provideRetrofitClient() =
         YourBackendHttpClientFactory.create(DemoPasswordlessOptions.YOUR_BACKEND_URL)
 }
