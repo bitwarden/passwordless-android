0e9f1d5787b9cd4d038487ec51cc6c684b1162d7
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-01-30 09:32:28 +0100

    Unused dependencies removal

--- passwordless/build.gradle.kts
+++ passwordless/build.gradle.kts
@@ -33,8 +33,6 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.google.code.gson:gson:2.10.1")
