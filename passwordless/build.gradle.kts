870b0ef18d8c8e1362ceae8444f4e8473bc5fe32
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-06-21 13:44:03 +0200

    Upgrade vanniktech publishing dependency

--- passwordless/build.gradle.kts
+++ passwordless/build.gradle.kts
@@ -4,7 +4,7 @@ import com.vanniktech.maven.publish.SonatypeHost
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
-    id("com.vanniktech.maven.publish") version "0.28.0"
+    id("com.vanniktech.maven.publish")
 }
 
 android {
