6bf43b63e44f6be4cdb472f76256456a200afbf4
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-07-05 14:18:18 +0200

    Fix tests

--- passwordless/src/test/java/dev/passwordless/android/rest/PasswordlessOptionsTests.kt
+++ passwordless/src/test/java/dev/passwordless/android/rest/PasswordlessOptionsTests.kt
@@ -12,16 +12,14 @@ class PasswordlessOptionsTests {
         // arrange
         val apiKey = "jonasandroid:public:ab2e4350d43946f7b4c93d98fa2c765e"
         val rpId = "example.com"
-        val origin = "android:apk-key-hash:POIplOLeHuvl-XAQckH0DwY4Yb1ydnnKcmhn-jibZbk"
         val apiUrl = "https://v4.passwordless.dev"
 
         // act
-        sut = PasswordlessOptions(apiKey, rpId, origin, apiUrl)
+        sut = PasswordlessOptions(apiKey, rpId, apiUrl)
 
         // assert
         assertEquals(apiKey, sut.apiKey)
         assertEquals(rpId, sut.rpId)
-        assertEquals(origin, sut.origin)
         assertEquals(apiUrl, sut.apiUrl)
     }
 
@@ -30,11 +28,10 @@ class PasswordlessOptionsTests {
         // arrange
         val apiKey = "jonasandroid:public:ab2e4350d43946f7b4c93d98fa2c765e"
         val rpId = "example.com"
-        val origin = "android:apk-key-hash:POIplOLeHuvl-XAQckH0DwY4Yb1ydnnKcmhn-jibZbk"
         val apiUrl = "https://v4.passwordless.dev"
 
         // act
-        sut = PasswordlessOptions(apiKey, rpId, origin)
+        sut = PasswordlessOptions(apiKey, rpId)
 
         // assert
         assertEquals(apiUrl, sut.apiUrl)
@@ -45,12 +42,11 @@ class PasswordlessOptionsTests {
         // arrange
         val apiKey = "jonasandroid:public:ab2e4350d43946f7b4c93d98fa2c765e"
         val rpId = ""
-        val origin = "android:apk-key-hash:POIplOLeHuvl-XAQckH0DwY4Yb1ydnnKcmhn-jibZbk"
         val apiUrl = "https://v4.passwordless.dev"
 
         // act
         assertThrows("rpId must not be blank", IllegalArgumentException::class.java) {
-            sut = PasswordlessOptions(apiKey, rpId, origin, apiUrl)
+            sut = PasswordlessOptions(apiKey, rpId, apiUrl)
         }
     }
 
@@ -59,12 +55,11 @@ class PasswordlessOptionsTests {
         // arrange
         val apiKey = "badkey"
         val rpId = "yourexample.com"
-        val origin = "android:apk-key-hash:POIplOLeHuvl-XAQckH0DwY4Yb1ydnnKcmhn-jibZbk"
         val apiUrl = "https://v4.passwordless.dev"
 
         // act
         assertThrows("apiKey must be a valid API key '<appname>:public:<uuid-without-dashes>'", IllegalArgumentException::class.java) {
-            sut = PasswordlessOptions(apiKey, rpId, origin, apiUrl)
+            sut = PasswordlessOptions(apiKey, rpId, apiUrl)
         }
     }
 
@@ -73,40 +68,11 @@ class PasswordlessOptionsTests {
         // arrange
         val apiKey = ""
         val rpId = "example.com"
-        val origin = "android:apk-key-hash:POIplOLeHuvl-XAQckH0DwY4Yb1ydnnKcmhn-jibZbk"
         val apiUrl = "https://v4.passwordless.dev"
 
         // act
         assertThrows("apiKey must not be blank", IllegalArgumentException::class.java) {
-            sut = PasswordlessOptions(apiKey, rpId, origin, apiUrl)
-        }
-    }
-
-    @Test
-    fun origin_throws_illegalArgumentException_whenIncorrectBase64Url() {
-        // arrange
-        val apiKey = "jonasandroid:public:ab2e4350d43946f7b4c93d98fa2c765e"
-        val rpId = "example.com"
-        val origin = "android:apk-key-hash:POIplOLeHuvl+XAQckH0DwY4Yb1ydnnKcmhn+jibZbk"
-        val apiUrl = "https://v4.passwordless.dev"
-
-        // act
-        assertThrows("origin must be a valid URL", IllegalArgumentException::class.java) {
-            sut = PasswordlessOptions(apiKey, rpId, origin, apiUrl)
-        }
-    }
-
-    @Test
-    fun origin_throws_illegalArgumentException_whenNotFacetId() {
-        // arrange
-        val apiKey = "jonasandroid:public:ab2e4350d43946f7b4c93d98fa2c765e"
-        val rpId = "example.com"
-        val origin = "https://yourbackend.com"
-        val apiUrl = "https://v4.passwordless.dev"
-
-        // act
-        assertThrows("origin must be a facet id 'android:apk-key-hash:base64url'", IllegalArgumentException::class.java) {
-            sut = PasswordlessOptions(apiKey, rpId, origin, apiUrl)
+            sut = PasswordlessOptions(apiKey, rpId, apiUrl)
         }
     }
 }
